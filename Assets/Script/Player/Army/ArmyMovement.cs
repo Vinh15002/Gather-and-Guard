334ccdf321ea9b37cf9bc86a9061fa41fb6e5b9a
= <vvvinhtran@gmail.com>
2025-03-02 15:59:39 +0700

    Add Line Up Horizontal + Vertical + Triangle

---
+++ Assets/Script/Player/Army/ArmyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class ArmyMovement : MonoBehaviour, IMovement
+{
+    [SerializeField] private float speed;
+
+    private ArmyPlayer units;
+
+    private Vector3 directionMovement;
+
+    private void Start()
+    {
+        units = GetComponent<ArmyPlayer>();
+    }
+
+
+
+    private void Update()
+    {
+        Movement();
+
+
+    }
+
+    private void Movement()
+    {
+        transform.Translate(directionMovement * Time.deltaTime*speed);
+        foreach (PlayerController contrl in units.players)
+        {
+            contrl.onMove(directionMovement);
+        }
+    }
+
+    public void OnMove(Vector3 direction)
+    {
+        directionMovement = direction;
+        //transform.Translate(direction*Time.deltaTime);
+    }
+
+
+}
--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour, IMovement
 {
 
-    [SerializeField] private float speed;
+   
 
     private Rigidbody rb;
     private Animator animator;
@@ -23,24 +23,15 @@ public class PlayerMovement : MonoBehaviour, IMovement
 
     private void Update()
     {
-        //GetInput();
-
 
         Movement();
     }
 
-    private void GetInput()
-    {
-        float dirX = Input.GetAxis("Horizontal");
-        float dirZ = Input.GetAxis("Vertical");
-
-        directionMovement = new Vector3(dirX,0, dirZ);
-
-    }
+  
 
     private void Movement()
     {
-        rb.velocity = directionMovement*speed;
+      
         bool isMove = directionMovement.magnitude > 0.2f;
         animator.SetBool("IsRun", isMove);
         if(isMove)

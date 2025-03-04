5f7dfa35d96aa422adbfe99f8ac577d01d7be7a6
= <vvvinhtran@gmail.com>
2025-03-04 16:21:19 +0700

    Add Enemy Goblin

---
+++ Assets/Script/Enemy/EnemyWeaponAttack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyWeaponAttack : MonoBehaviour
+{
+    [SerializeField] private float _timeDealDame = 1f;
+
+    private bool canAttack => _timeDealDame <= 0;
+
+
+
+
+    private void Update()
+    {
+        DecreaseTimeAttack();
+    }
+
+    private void DecreaseTimeAttack()
+    {
+       
+        if(_timeDealDame > 0)
+        {
+            _timeDealDame -= Time.deltaTime;
+        }
+    }
+
+
+    protected virtual void OnTriggerStay(Collider other)
+    {
+    
+        if (other.CompareTag("Player") && canAttack)
+        {
+            Debug.Log("Goblin Deal Damage");
+        }
+    }
+
+
+}
